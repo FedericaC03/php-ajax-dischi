1be22f7efdc7ae62a8bc6908ad1c6eacece24e6b
FedericaC03 <fdrk03@hotmail.it>
2020-10-07 17:33:22 +0200

    header + main

--- index.php
+++ index.php
@@ -1,15 +1,69 @@
+<?php include_once("db.php") ?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="dist/app.css">
     <title></title>
   </head>
   <body>
     <header>
-
+      <div class="container">
+          <img src="src\logo.png" alt="logo">
+      </div>
     </header>
     <main>
+      <div class="row">
+        <div class="disc">
+          <div class="poster">
+            <img src="https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg" alt="">
+          </div>
+          <div class="title">
+            <h2>New Jersey</h2>
+          </div>
+          <div class="artist">
+            <p>Bon Jovi <br> 1988</p>
+          </div>
+        </div>
 
+        <div class="disc">
+          <div class="poster">
+            <img src="https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg" alt="">
+          </div>
+          <div class="title">
+            <h2>New Jersey</h2>
+          </div>
+          <div class="artist">
+            <p>Bon Jovi <br> 1988</p>
+          </div>
+        </div>
+
+        <div class="disc">
+          <div class="poster">
+            <img src="https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg" alt="">
+          </div>
+          <div class="title">
+            <h2>New Jersey</h2>
+          </div>
+          <div class="artist">
+            <p>Bon Jovi <br> 1988</p>
+          </div>
+        </div>
+
+        <div class="disc">
+          <div class="poster">
+            <img src="https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg" alt="">
+          </div>
+          <div class="title">
+            <h2>New Jersey</h2>
+          </div>
+          <div class="artist">
+            <p>Bon Jovi <br> 1988</p>
+          </div>
+        </div>
+
+      </div>
     </main>
   </body>
 </html>
