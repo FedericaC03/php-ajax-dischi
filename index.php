6a3e23899aec0640b63affc397f7ebea6393a524
FedericaC03 <fdrk03@hotmail.it>
2020-10-07 17:44:26 +0200

    fine milestone 1

--- index.php
+++ index.php
@@ -15,54 +15,19 @@
     </header>
     <main>
       <div class="row">
+        <?php foreach ($database as $disc): ?>
         <div class="disc">
-          <div class="poster">
-            <img src="https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg" alt="">
-          </div>
-          <div class="title">
-            <h2>New Jersey</h2>
-          </div>
-          <div class="artist">
-            <p>Bon Jovi <br> 1988</p>
-          </div>
+            <div class="poster">
+              <img src="<?php echo $disc["poster"]; ?>" alt="poster">
+            </div>
+            <div class="title">
+              <h2> <?php echo $disc["title"]; ?></h2>
+            </div>
+            <div class="artist">
+              <p> <?php echo $disc["author"]; ?> <br> <?php echo $disc["year"]; ?></p>
+            </div>
         </div>
-
-        <div class="disc">
-          <div class="poster">
-            <img src="https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg" alt="">
-          </div>
-          <div class="title">
-            <h2>New Jersey</h2>
-          </div>
-          <div class="artist">
-            <p>Bon Jovi <br> 1988</p>
-          </div>
-        </div>
-
-        <div class="disc">
-          <div class="poster">
-            <img src="https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg" alt="">
-          </div>
-          <div class="title">
-            <h2>New Jersey</h2>
-          </div>
-          <div class="artist">
-            <p>Bon Jovi <br> 1988</p>
-          </div>
-        </div>
-
-        <div class="disc">
-          <div class="poster">
-            <img src="https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg" alt="">
-          </div>
-          <div class="title">
-            <h2>New Jersey</h2>
-          </div>
-          <div class="artist">
-            <p>Bon Jovi <br> 1988</p>
-          </div>
-        </div>
-
+      <?php endforeach ?>
       </div>
     </main>
   </body>
