331a4375f256cb640f4004ac9c8d4cfa75aaa2f6
FedericaC03 <fdrk03@hotmail.it>
2020-10-08 13:52:53 +0200

    installazione di jquery e handlebars

--- index.php
+++ index.php
@@ -30,5 +30,18 @@
       <?php endforeach ?>
       </div>
     </main>
+
+
+    <!-- TEMPLATE -->
+    <script id="entry-template" type="text/x-handlebars-template">
+        <div class="entry">
+          <h1>{{title}}</h1>
+          <div class="body">
+            {{body}}
+          </div>
+        </div>
+    </script>
+    <!-- END TEMPLATE -->
+    <script src="dist/app.js" charset="utf-8"></script>
   </body>
 </html>
---
+++ src/app.js
@@ -0,0 +1,22 @@
+const $ = require('jquery');
+const Handlebars = require("handlebars");
+
+$(document).ready(function(){
+
+
+alert();
+
+
+
+
+});
+
+
+//HANDLEBARS
+// var source = document.getElementById("entry-template").innerHTML;
+// var template = Handlebars.compile(source);
+// var context = {
+  //     "title": "My New Post",
+  //     "body": "This is my first post!"
+  // };
+  // var html = template(context);
