fdc58e3fc9ef633aaa90d66e552d4aeeac1f1e7e
FedericaC03 <fdrk03@hotmail.it>
2020-10-08 17:43:22 +0200

    FINE ESERCIZIO

--- src/app.js
+++ src/app.js
@@ -1,35 +1,36 @@
 //
 $(document).ready(function()  {
-//   const $ = require('jquery');
-//   const Handlebars = require("handlebars");
 
-//CHIAMATA AJAX
-// $.ajax({
-//
-//   "url": "http://localhost/php-ajax-dischi/src/partials/api.php",
-//   "method": "GET",
-//   "success": function(data){
-//     console.log(data);
-//   },
-//   "error": function(err){
-//     alert("errore");
-//   },
-//
-// )};
+// CHIAMATA AJAX
+$.ajax({
 
-alert();
-
-
-
-
+  "url": "http://localhost/php-ajax-dischi/src/partials/api.php",
+  "method": "GET",
+  "success": function(data){
+    render(data);
+  },
+  "error": function(err){
+    alert("errore");
+  },
+});
 });
 
+//FUNCTION PER CICLARE TUTTI I DISCHI
+function render(result) {
+  for (var i = 0; i < result.length; i++) {
+    console.log(result[i]["title"]);
 
-//HANDLEBARS
-// var source = document.getElementById("entry-template").innerHTML;
-// var template = Handlebars.compile(source);
-// var context = {
-  //     "title": "My New Post",
-  //     "body": "This is my first post!"
-  // };
-  // var html = template(context);
+    //HANDLEBARS
+    var source = $("#entry-template").html();
+    var template = Handlebars.compile(source);
+
+    var context = {
+        "title": result[i]["title"],
+        "author": result[i]["author"],
+        "year": result[i]["year"],
+        "poster": result[i]["poster"]
+    };
+    var html = template(context);
+    $(".row").append(html);
+  }
+}
--- src/partials/index.php
+++ src/partials/index.php
@@ -2,6 +2,8 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+     <script src="https://cdn.jsdelivr.net/npm/handlebars@latest/dist/handlebars.js"></script>
     <link rel="stylesheet" href="../../dist/app.css">
     <title>Dischi</title>
   </head>
@@ -20,12 +22,17 @@
 
     <!-- TEMPLATE -->
     <script id="entry-template" type="text/x-handlebars-template">
-        <div class="entry">
-          <h1>{{title}}</h1>
-          <div class="body">
-            {{body}}
+      <div class="disc">
+          <div class="poster">
+            <img src="{{poster}}" alt="poster">
           </div>
-        </div>
+          <div class="title">
+            <h2>{{title}}</h2>
+          </div>
+          <div class="artist">
+            <p> {{author}} <br> {{year}}</p>
+          </div>
+      </div>
     </script>
     <!-- END TEMPLATE -->
     <script src="../app.js" charset="utf-8"></script>
